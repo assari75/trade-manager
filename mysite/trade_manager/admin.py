3edb6ce613b0561a15f6f119d1f293500c03b42d
Majid <m.asari1375@gmail.com>
2023-04-07 01:45:24 +0330

    feat: add trade model to admin panel

--- mysite/trade_manager/admin.py
+++ mysite/trade_manager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class TradeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "symbol",
+        "price",
+        "side",
+        "time_stamp",
+    )
+    list_filter = (
+        "symbol",
+        "side",
+    )
+
+admin.site.register(models.Trade, TradeAdmin)
